7879fd8ece77d63d88be30fe9c2174c5fabd7cd6
Mustafa Soyvural <soyvural@gmail.com>
2021-11-03 03:45:14 +0000

    build script added

---
+++ build/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# exit immediately when an error occurs.
+set -e
+
+BUILD_VERSION=$1
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+echo "contanier build with version $BUILD_VERSION"
+
+docker build -f $GIT_ROOT/build/Dockerfile -t soyvural/simple-device-api:$BUILD_VERSION .
+echo $DOCKER_PASSWORD | docker login registry-1.docker.io --username $DOCKER_USERNAME --password-stdin
+docker push soyvural/simple-device-api:$BUILD_VERSION
